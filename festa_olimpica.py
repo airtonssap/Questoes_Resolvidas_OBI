0f42176b319bdaf20d274be9e9b6da4498994db8
airtonssap <104590505+airtonssap@users.noreply.github.com>
2022-05-18 09:29:52 -0300

    Update festa_olimpica.py

--- festa_olimpica.py
+++ festa_olimpica.py
@@ -4,16 +4,14 @@ c = list(range(1, convidados+1))
 
 #corte
 sorteios = int(input('Quantidade de sorteios para corte: '))
-soma = 0
 for i in range(sorteios):
     s = int(input('Número sorteado: '))
-    for removidos in range(s, convidados+1, s):
-        if removidos == s:
-            soma = 0
-        c.pop(removidos-1 - soma)
-        soma += 1
-    convidados -= soma
+    del c[s-1::s]
 
 #saída
+soma = 0
 for valor in c:
     print(valor)
+    soma +=1
+    if soma == 10000:
+        break
